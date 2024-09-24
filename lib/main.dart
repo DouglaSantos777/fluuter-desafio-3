d589dbb6a681b8c70a706d9b5506f59f182c64f0
Douglas Santos <159207868+DouglaSantos777@users.noreply.github.com>
2024-09-24 10:21:04 -0300

    atualização

--- lib/main.dart
+++ lib/main.dart
@@ -31,142 +31,163 @@ class VolleyPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 4),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/ball.png',
-                  width: 42,
-                  height: 42,
-                  fit: BoxFit.cover,
-                ),
-                const SizedBox(width: 15),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Volley",
-                      style: TextStyle(
-                        fontSize: 42,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Transform.translate(
-                      offset: const Offset(-7, -13),
-                      child: const Text(
-                        "do fim de semana",
-                        style: TextStyle(
-                          fontSize: 9,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 38),
-            Center(
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    width: 60,
-                    height: 205,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 2),
-                      color: const Color(0XFF73CFD5),
-                    ),
-                    child: const RotatedBox(
-                      quarterTurns: 3,
-                      child: Center(
-                        child: Text(
-                          'TIMES',
+                  Image.asset(
+                    'assets/images/ball.png',
+                    width: 42,
+                    height: 42,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 15),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Volley",
+                        style: TextStyle(
+                          fontSize: 42,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Transform.translate(
+                        offset: const Offset(-7, -13),
+                        child: const Text(
+                          "do fim de semana",
                           style: TextStyle(
+                            fontSize: 9,
                             color: Colors.white,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  const SizedBox(width: 20),
-                  Expanded(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        _buildTeamRow('Sicranos', '3'),
-                        const SizedBox(height: 2),
-                        _buildTeamRow('Autoconvidados', '3'),
-                        const SizedBox(height: 2),
-                        _buildTeamRow('Ziraldos', '4'),
-                        const SizedBox(height: 2),
-                        _buildTeamRow('Sparrings', '5'),
-                      ],
-                    ),
+                    ],
                   ),
                 ],
               ),
             ),
-            const SizedBox(height: 33),
-            Column(
+            const Spacer(),
+
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
+                  width: 60,
+                  height: 190,
                   decoration: BoxDecoration(
-                    color: Colors.transparent,
+                    border: Border.all(color: Colors.white, width: 2),
+                    color: Color(0XFF73CFD5),
                   ),
-                  child: TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'Jogo Casado',
-                      style: TextStyle(color: Colors.white, fontSize: 27),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0XFF2B4A8E),
-                    border: Border.all(color: Colors.white, width: 3),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 5.0, horizontal: 54.0),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Iniciar',
+                  child: const RotatedBox(
+                    quarterTurns: 3,
+                    child: Center(
+                      child: Text(
+                        'TIMES',
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 30,
+                          fontSize: 35,
+                          fontWeight: FontWeight.w600,
                         ),
                       ),
                     ),
                   ),
                 ),
+                const SizedBox(width: 20),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      _buildTeamRow('Sicranos', '3'),
+                      _buildTeamRow('Autoconvidados', '3'),
+                      _buildTeamRow('Ziraldos', '4'),
+                      _buildTeamRow('Sparrings', '5'),
+                    ],
+                  ),
+                ),
               ],
             ),
-            const SizedBox(height: 85),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: FloatingActionButton(
-                onPressed: () {},
-                backgroundColor:
-                    const Color(0XFF2B4A8E),
-                    foregroundColor: Colors.white, 
-                child: const Icon(Icons.add),
+            const Spacer(),
+
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Jogo Casado',
+                style: TextStyle(color: Colors.white, fontSize: 27),
               ),
             ),
+            const SizedBox(height: 15),
+
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(0XFF2B4A8E),
+                border: Border.all(color: Colors.white, width: 3),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5.0, horizontal: 54.0),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Iniciar',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(),
+
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    right: 16.0,
+                    bottom: 16.0), 
+                child: SizedBox(
+                  width: 45,
+                  height: 45,
+                  child: FloatingActionButton(
+                    onPressed: () {},
+                    backgroundColor: const Color(0XFF2B4A8E),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          width: 12,
+                          height: 4,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(2),
+                          ),
+                        ),
+                        Container(
+                          width: 4,
+                          height: 12,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(2),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
@@ -179,7 +200,10 @@ class VolleyPage extends StatelessWidget {
       children: [
         Text(
           teamName,
-          style: const TextStyle(color: Color(0XFFEDC735), fontSize: 30),
+          style: const TextStyle(
+            color: Color(0XFFEDC735), 
+            fontSize: 26,
+            height: 1),
         ),
         const SizedBox(width: 15),
         Padding(
@@ -188,7 +212,10 @@ class VolleyPage extends StatelessWidget {
             children: [
               Text(
                 playerCount,
-                style: const TextStyle(color: Color(0XFF2B4A8E), fontSize: 48),
+                style: const TextStyle(color: Color(0XFF2B4A8E), 
+                fontSize: 45,
+                height: 1
+                ),
               ),
               const RotatedBox(
                 quarterTurns: 3,
